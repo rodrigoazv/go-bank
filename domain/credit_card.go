2b1f43f6b536fbf6b61280240737fee17b025beb
rodrigoazv <rodrigaocma@gmail.com>
2021-09-12 15:36:46 -0300

    feat: adding create and get credit card functions

--- domain/credit_card.go
+++ domain/credit_card.go
@@ -7,15 +7,15 @@ import (
 )
 
 type CreditCard struct {
-	ID               string
-	Name             string
-	Number           string
-	ExpirantionMonth int32
-	ExpirantionYear  int32
-	CVV              int32
-	Balance          float64
-	Limit            float64
-	CreatedAt        time.Time
+	ID              string
+	Name            string
+	Number          string
+	ExpirationMonth int32
+	ExpirationYear  int32
+	CVV             int32
+	Balance         float64
+	Limit           float64
+	CreatedAt       time.Time
 }
 
 func NewCreditCard() CreditCard {
--- infra/repository/transaction.go
+++ infra/repository/transaction.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/rodrigoazv/go-bank/domain"
 )
@@ -53,4 +54,41 @@ func (t *TransactionRepositoryDb) updateBalance(creditCard domain.CreditCard) er
 		return err
 	}
 	return nil
+}
+func (t *TransactionRepositoryDb) CreateCreditCard(creditCard domain.CreditCard) error {
+	stmt, err := t.db.Prepare(`insert into credit_cards(id, name, number, expiration_month,expiration_year, CVV,balance, balance_limit) 
+								values($1,$2,$3,$4,$5,$6,$7,$8)`)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(
+		creditCard.ID,
+		creditCard.Name,
+		creditCard.Number,
+		creditCard.ExpirationMonth,
+		creditCard.ExpirationYear,
+		creditCard.CVV,
+		creditCard.Balance,
+		creditCard.Limit,
+	)
+	if err != nil {
+		return err
+	}
+	err = stmt.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TransactionRepositoryDb) GetCreditCard(creditCard domain.CreditCard) (domain.CreditCard, error) {
+	var c domain.CreditCard
+	stmt, err := t.db.Prepare("select id, balance, balance_limit from credit_cards where number=$1")
+	if err != nil {
+		return c, err
+	}
+	if err = stmt.QueryRow(creditCard.Number).Scan(&c.ID, &c.Balance, &c.Limit); err != nil {
+		return c, errors.New("credit card does not exists")
+	}
+	return c, nil
 }
