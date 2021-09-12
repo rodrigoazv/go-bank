7f8d75c6992d2b1ac080c992f854e904748d5507
rodrigoazv <rodrigaocma@gmail.com>
2021-09-12 15:13:31 -0300

    feat: adding infra layer and transaction repository

---
+++ infra/repository/transaction.go
@@ -0,0 +1,11 @@
+package repository
+
+import "database/sql"
+
+type TransactionRepositoryDb struct {
+	db *sql.DB
+}
+
+func NewTransactionRepositoryDb(db *sql.DB) *TransactionRepositoryDb {
+	return &TransactionRepositoryDb{db: db}
+}
