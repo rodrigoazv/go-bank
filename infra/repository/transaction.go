03aee6bc47f53170967a498109cf575649913088
rodrigoazv <rodrigaocma@gmail.com>
2021-09-12 15:34:55 -0300

    feat: adding transaction logical without orm

--- infra/repository/transaction.go
+++ infra/repository/transaction.go
@@ -1,6 +1,10 @@
 package repository
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/rodrigoazv/go-bank/domain"
+)
 
 type TransactionRepositoryDb struct {
 	db *sql.DB
@@ -8,4 +12,45 @@ type TransactionRepositoryDb struct {
 
 func NewTransactionRepositoryDb(db *sql.DB) *TransactionRepositoryDb {
 	return &TransactionRepositoryDb{db: db}
+}
+
+func (t *TransactionRepositoryDb) SaveTransaction(transaction domain.Transaction, creditCard domain.CreditCard) error {
+	stmt, err := t.db.Prepare(`insert into transactions(id, credit_card_id, amount, status, description, store, created_at) values($1, $2, $3, $4, $5, $6, $7)`)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(
+		transaction.ID,
+		transaction.CreditCardId,
+		transaction.Amount, transaction.Status,
+		transaction.Description,
+		transaction.Store,
+		transaction.CreatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	if transaction.Status == "aproved" {
+		err = t.updateBalance(creditCard)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TransactionRepositoryDb) updateBalance(creditCard domain.CreditCard) error {
+	_, err := t.db.Exec("update credit_cards set balance = $1 where id = $2", creditCard.Balance, creditCard.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
