f378b81e17106fcc14397480bb57576d40dbf650
rodrigoazv <rodrigozcma@gmail.com>
2021-09-16 07:47:45 -0300

    feat: adding setupTransaction main layer with grpc and kafka

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	_ "github.com/lib/pq"
+	"github.com/rodrigoazv/go-bank/infra/grpc/server"
+	"github.com/rodrigoazv/go-bank/infra/kafka"
 	"github.com/rodrigoazv/go-bank/infra/repository"
 	"github.com/rodrigoazv/go-bank/usecases"
 )
@@ -14,12 +16,25 @@ func main() {
 	db := setupDb()
 	defer db.Close()
 
+	producer := setupKakfa()
+	processTransactionUseCase := setupTransactionUseCase(db, producer)
+
+	fmt.Println("Rodando grp")
+
+	serveGrpc(processTransactionUseCase)
+
 }
 
-func setupTransactionUseCase(db *sql.DB) usecases.UseCaseTransaction {
+func setupTransactionUseCase(db *sql.DB, producer kafka.KafkaProducer) usecases.UseCaseTransaction {
 	transactionRepository := repository.NewTransactionRepositoryDb(db)
 	useCase := usecases.NewUseCaseTransaction(transactionRepository)
+	useCase.KafkaProducer = producer
 	return useCase
+}
+func setupKakfa() kafka.KafkaProducer {
+	producer := kafka.NewKafkaProducer()
+	producer.SetupProducer("host.docker.internal:9094")
+	return producer
 }
 
 func setupDb() *sql.DB {
@@ -37,4 +52,11 @@ func setupDb() *sql.DB {
 	}
 
 	return db
+}
+
+func serveGrpc(processTransactionUseCase usecases.UseCaseTransaction) {
+	grpcServer := server.NewGRPCServer()
+	grpcServer.ProcessTransactionUseCase = processTransactionUseCase
+
+	grpcServer.Serve()
 }
